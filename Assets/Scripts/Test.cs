a2dd374d61e679f7de9f667f90e6d43e625e1004
Suphanat1722 <suphanat1722@gmail.com>
2024-07-21 16:18:18 +0700

    เปิดปิดราคากลาง
    ทำระบบกดปุ่มเพื่อเปิดปิดหน้าต่างราคากลางเสร็จแล้ว

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -11,6 +11,9 @@ public class Test : MonoBehaviour
     private int reliability = 10;
     private int nervous = 10;
 
+    public GameObject averagePrice;
+    private bool isPressAPbutton;
+
     void Start()
     {
         cerrentTurn = 1;
@@ -19,6 +22,28 @@ public class Test : MonoBehaviour
     
     void Update()
     {
+        if (isPressAPbutton)
+        {
+            averagePrice.SetActive(true);
+        }
+        else
+        {
+            averagePrice.SetActive(false);
+        }
         
     }
+
+    public void PressAveragePriceButton()
+    {
+        if (!isPressAPbutton)
+        {
+            isPressAPbutton = true;
+        }
+        else
+        {
+            isPressAPbutton = false;
+        }  
+    }
+
+
 }
