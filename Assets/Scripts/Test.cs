db25f9d334dc23cca4f463b1a6a3053514764fdd
Suphanat1722 <suphanat1722@gmail.com>
2024-07-21 16:03:04 +0700

    Setup project
    - add font thai
    - add average price Button
    -add average price

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int cerrentTurn; // 1-4
+
+    private int halth = 10;
+    private int bargain = 10;
+    private int reliability = 10;
+    private int nervous = 10;
+
     void Start()
     {
-        
+        cerrentTurn = 1;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
